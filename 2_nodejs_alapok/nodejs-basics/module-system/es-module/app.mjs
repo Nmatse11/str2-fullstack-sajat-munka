fc3455161279ea59d441346b5896eb61abb398b0
Nmatse <atseniko@gmail.com>
2022-04-22 00:53:01 +0200

    works

---
+++ 2_nodejs_alapok/nodejs-basics/module-system/es-module/app.mjs
@@ -0,0 +1,8 @@
+import * as utils from './utils.mjs'
+// import { even, odd } from './utils.mjs'
+
+console.log(utils)
+
+const priceList = [345, 67, 234, 57, 236, 456]
+console.log(utils.even(priceList))
+console.log(utils.odd(priceList))
